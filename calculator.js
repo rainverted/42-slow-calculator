8ac872ff1b2bf3be183cd5de98c7a2e09b1381c7
rainverted <MY_NAME@example.com>
2021-08-02 19:23:50 +0300

    septynetai

--- calculator.js
+++ calculator.js
@@ -1,4 +1,9 @@
-async function calculator() {
+async function calculator(num1, num2) {
+    const sum = num1 + num2;
+
+    return new Promise((resolve) => {
+        resolve(sum);
+    })
 }
 
 module.exports = calculator;
